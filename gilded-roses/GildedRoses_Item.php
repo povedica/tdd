98d34fec1d71ef269367f3caad896ed11c59218c
povedica <yoyyosemite@gmail.com>
2018-03-18 16:25:37 +0100

    - The Quality of an item is never negative

--- gilded-roses/GildedRoses_Item.php
+++ gilded-roses/GildedRoses_Item.php
@@ -12,7 +12,7 @@ class GildedRoses_Item
     public function __construct($sellin, $quantity)
     {
         $this->sellin = $sellin;
-        $this->quantity = $quantity;
+        $this->set_quantity($quantity);
     }
 
     public function decrease_sellin()
@@ -20,10 +20,15 @@ class GildedRoses_Item
         $this->sellin = $this->sellin - self::DECREASE_SELLIN_UNITS_BY_DAY;
     }
 
+    public function set_quantity($quantity)
+    {
+        $this->quantity = ($this->quantity < 0) ? 0 : $quantity;
+    }
+
     public function decrease_quantity()
     {
         $multiplier_factor = ($this->get_sellin() <= 0) ? self::MULTIPLIER_FACTOR_WHEN_SELLIN_HAS_PASSED : self::DECREASE_QUANTITY_UNITS_BY_DAY;
-        $this->quantity = $this->quantity - ((self::DECREASE_QUANTITY_UNITS_BY_DAY) * $multiplier_factor);
+        $this->set_quantity($this->quantity - ((self::DECREASE_QUANTITY_UNITS_BY_DAY) * $multiplier_factor));
     }
 
     public function get_sellin()
--- gilded-roses/test/Test_GildedRoses_Item.php
+++ gilded-roses/test/Test_GildedRoses_Item.php
@@ -14,7 +14,7 @@ class Test_GildedRoses_Item extends TestCase
 
     public function setUp()
     {
-        $this->item = new GildedRoses_Item(10,10);
+        $this->item = new GildedRoses_Item(10, 10);
     }
 
     public function test_decrease_one_day_sellin_is_ok()
@@ -32,8 +32,15 @@ class Test_GildedRoses_Item extends TestCase
 
     public function test_decrease_twice_faster_if_sellin_has_passed()
     {
-        $this->item = new GildedRoses_Item(0,10);
+        $this->item = new GildedRoses_Item(0, 10);
         $this->item->decrease_quantity();
         $this->assertEquals(8, $this->item->get_quantity());
+    }
+
+    public function test_quality_is_never_negative()
+    {
+        $this->item = new GildedRoses_Item(10, -1);
+        $this->item->decrease_quantity();
+        $this->assertEquals(0, $this->item->get_quantity());
     }
 }
