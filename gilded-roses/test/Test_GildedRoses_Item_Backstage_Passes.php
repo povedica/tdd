4763084fe496abffd41afb731693b4e8988c9d53
povedica <yoyyosemite@gmail.com>
2018-03-18 18:00:45 +0100

    Refactoring tests. Each class has its own test class

---
+++ gilded-roses/test/Test_GildedRoses_Item_Backstage_Passes.php
@@ -0,0 +1,77 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once '../GildedRoses_Item.php';
+require_once '../GildedRoses_Item_Backstage_Passes.php';
+
+/**
+ * Class Test_GildedRoseItem
+ */
+class Test_GildedRoses_Item_Backstage_Passes extends TestCase
+{
+    /** @var GildedRoses_Item */
+    private $item;
+
+    public function setUp()
+    {
+        $this->item = new GildedRoses_Item_Backstage_Passes(10, 10);
+    }
+
+    /**
+     * @param $n
+     * @param $expected
+     * @dataProvider provider_data_for_test_backstages_pases_increase_value_as_its_sellin_approaches
+     */
+    public function test_backstages_pases_increase_value_as_its_sellin_approaches($n,$expected)
+    {
+        $this->item = new GildedRoses_Item_Backstage_Passes($n, $n);
+        $this->item->decrease_sellin();
+        $this->assertEquals($expected, $this->item->get_quality());
+    }
+
+    /**
+     * @param $n
+     * @param $expected
+     * @dataProvider provider_data_for_test_backstages_pases_increase_value_by_three_when_less_than_6_days_to_sellin
+     */
+    public function test_backstages_pases_increase_value_by_three_when_less_than_6_days_to_sellin($n,$expected)
+    {
+        $this->item = new GildedRoses_Item_Backstage_Passes($n, $n);
+        $this->item->decrease_sellin();
+        $this->assertEquals($expected, $this->item->get_quality());
+    }
+
+    public function test_quality_drops_to_zero_when_sellin_has_passed(){
+        $this->item = new GildedRoses_Item_Backstage_Passes(1,10);
+        $this->item->decrease_sellin();
+        $this->assertEquals(0,$this->item->get_quality());
+    }
+
+    /**
+     * Provide set of data to test cases
+     * @return array
+     */
+    public function provider_data_for_test_backstages_pases_increase_value_as_its_sellin_approaches()
+    {
+        return array(
+            array(10, 12),
+            array(11, 13),
+            array(12, 13)
+        );
+    }
+
+    /**
+     * Provide set of data to test cases
+     * @return array
+     */
+    public function provider_data_for_test_backstages_pases_increase_value_by_three_when_less_than_6_days_to_sellin()
+    {
+        return array(
+            array(6, 9),
+            array(5, 8),
+            array(7, 9),
+            array(20, 21)
+        );
+    }
+}
---
+++ gilded-roses/test/Test_GildedRoses_Item_Sulfura.php
@@ -0,0 +1,26 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once '../GildedRoses_Item.php';
+require_once '../GildedRoses_Item_Sulfura.php';
+
+/**
+ * Class Test_GildedRoseItem
+ */
+class Test_GildedRoses_Item_Sulfura extends TestCase
+{
+    /** @var GildedRoses_Item */
+    private $item;
+
+    public function setUp()
+    {
+        $this->item = new GildedRoses_Item_Sulfura(10, 10);
+    }
+
+    public function test_sulfuras_item_never_decrease_sellin()
+    {
+        $this->item = new GildedRoses_Item_Sulfura(10);
+        $this->assertEquals(10, $this->item->get_quality());
+    }
+}
