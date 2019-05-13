4ac1577e2e1f20bb4a9ff2c1b466c58cc01c54f1
povedica <yoyyosemite@gmail.com>
2019-05-13 20:17:57 +0200

    add: more code for Kata luhn

--- luhn/LuhnValidator.php
+++ luhn/LuhnValidator.php
@@ -1,16 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: pablo
- * Date: 28/11/18
- * Time: 0:06
- */
 
 class LuhnValidator {
-
-	/**
-	 * LuhnValidator constructor.
-	 */
-	public function __construct() {
+	public function isValid() {
+		return true;
 	}
 }
--- luhn/test/Test_Luhn.php
+++ luhn/test/Test_Luhn.php
@@ -1,13 +1,11 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: pablo
- * Date: 27/11/18
- * Time: 23:58
- */
+<?php declare ( strict_types=1 );
 
 use PHPUnit\Framework\TestCase;
+require_once '../LuhnValidator.php';
 
 class Test_Luhn extends TestCase {
-
+	public function testShouldValidateAllZeros(): void {
+		$validator = new LuhnValidator();
+		$this->assertTrue( $validator->isValid( '00000000000' ) );
+	}
 }
